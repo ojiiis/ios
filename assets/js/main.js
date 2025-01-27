603a4b16efea95ac3d358ab47a51ad0ff813b05c
ojiiis <official.ojingirisamuel@gmail.com>
2025-01-27 10:44:08 +0100

    27-10

--- assets/js/main.js
+++ assets/js/main.js
@@ -9,4 +9,26 @@
         document.getElementById("show-app").onclick = () => {
             document.getElementById("hero").scrollIntoView();
         }
+        if(document.getElementById("mobile-menu-toggle")){
+            document.getElementById("mobile-menu-toggle").onclick = function(){
+                if(document.getElementById("d-nav").style.display == "" || document.getElementById("d-nav").style.display == "none"){
+                      document.getElementById("d-nav").style.display = "flex";
+                      document.getElementById("toggle-icon").classList.remove("fa-bars");
+                      document.getElementById("toggle-icon").classList.add("fa-times");
+                }else{
+                     document.getElementById("d-nav").style.display = "";
+                     document.getElementById("toggle-icon").classList.remove("fa-times");
+                      document.getElementById("toggle-icon").classList.add("fa-bars");
+                }
+                 //console.log()
+            }
+        }
+        var dm = document.querySelectorAll("#d-nav li");
+        for(let i = 0; i < dm.length; i++){
+            dm[i].onclick = function(){
+                  document.getElementById("d-nav").style.display = "";
+                     document.getElementById("toggle-icon").classList.remove("fa-times");
+                      document.getElementById("toggle-icon").classList.add("fa-bars");
+            }
+        }
 
